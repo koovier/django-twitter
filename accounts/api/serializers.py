509208e7d647ed9526f25ace8429785a51fd7e40
Lancer523 <85416035+Lancer523@users.noreply.github.com>
2021-11-23 16:40:32 -0500

    add login, logout and log status function

--- accounts/api/serializers.py
+++ accounts/api/serializers.py
@@ -1,8 +1,13 @@
-from django.contrib.auth.models import User, Group
+from django.contrib.auth.models import User
 from rest_framework import serializers
 
 
 class UserSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = User
-        fields = ['url', 'username', 'email']
+        fields = ['id','username', 'email']
+
+
+class LoginSerializer(serializers.Serializer):
+    username = serializers.CharField()
+    password = serializers.CharField()
