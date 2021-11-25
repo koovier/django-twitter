84c73ec447efc932a541873fc2eef74103714ec6
Lancer523 <85416035+Lancer523@users.noreply.github.com>
2021-11-25 09:49:13 -0500

    install django debug toolbar.

--- accounts/api/views.py
+++ accounts/api/views.py
@@ -30,7 +30,10 @@ class AccountViewSet(viewsets.ViewSet):
 
     @action(methods=['GET'], detail=False)
     def login_status(self, request):
-        data = {'has_logged_in': request.user.is_authenticated}
+        data = {
+            'has_logged_in': request.user.is_authenticated,
+            'ip': request.META['REMOTE_ADDR'],
+        }
         if request.user.is_authenticated:
             data['user'] = UserSerializer(request.user).data
         return Response(data)
