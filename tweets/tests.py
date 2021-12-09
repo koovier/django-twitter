613ad3e21546dab53d98aaf55788e071ae2b0259
Lancer523 <85416035+Lancer523@users.noreply.github.com>
2021-12-09 12:34:16 -0500

    Update tests.py
    create unit test case

---
+++ tweets/tests.py
@@ -0,0 +1,18 @@
+from django.test import TestCase
+from django.contrib.auth.models import User
+from tweets.models import Tweet
+from datetime import timedelta
+from utils.time_helpers import utc_now
+
+
+class TweetTests(TestCase):
+
+    def test_hours_to_now(self):
+        sheldon = user.objects.create_user(username='sheldon')
+        tweet = Tweet.objects.create(user=sheldon, content='Bazinga!')
+        tweet.created_at = utc_now() - timedelta(hours=10)
+        tweet.save()
+        self.assertEqual(tweet.hours_to_now, 10)
+
+
+
